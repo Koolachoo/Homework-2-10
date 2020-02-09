06f9043d2ce5895c5ea5f62b0feb0ffe5881e11e
Justin Taylor <kiradez211@gmail.com>
2020-02-09 18:56:45 -0500

    Added function for genreation and prompts

--- Develop/script.js
+++ Develop/script.js
@@ -10,5 +10,28 @@ function writePassword() {
 
 }
 
+
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// prompt and confirms for user input
+
+prompt("What is the length of your password?");
+confirm("Do you want lowercase characters");
+confirm("Do you want uppercase characters");
+confirm("Do you want numeric characters");
+confirm("Do you want special characters");
+alert("Then click Generate Password");
+
+// loop that contains the string for all of the characters
+function generatePassword() {
+  var passLength = 9,
+      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*",
+      charVal = "";
+  for (var i = 0, n = charset.passLength; i < passLength; ++i) {
+      charVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return charVal;
+}
