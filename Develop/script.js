92f6ba63ec038c9d1f7932444c7333353d33b578
Justin Taylor <kiradez211@gmail.com>
2020-02-09 21:55:35 -0500

    Added prompts and confirms into the top function

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
+
 // Write password to the #password input
 function writePassword() {
+  answer = prompt("What is the length of your password?");
+   lcConfirm = confirm("Do you want lowercase characters");
+   upConfirm = confirm("Do you want uppercase characters");
+   numConfirm = confirm("Do you want numeric characters");
+   spConfirm = confirm("Do you want special characters");
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -10,7 +18,7 @@ function writePassword() {
 
 }
 
-
+// var lPrompt = var lcConfirm = var upConfirm = var numConfirm = var spConfirm = 
 
 
 // Add event listener to generate button
@@ -18,20 +26,17 @@ generateBtn.addEventListener("click", writePassword);
 
 // prompt and confirms for user input
 
-prompt("What is the length of your password?");
-confirm("Do you want lowercase characters");
-confirm("Do you want uppercase characters");
-confirm("Do you want numeric characters");
-confirm("Do you want special characters");
-alert("Then click Generate Password");
+
+
 
 // loop that contains the string for all of the characters
 function generatePassword() {
-  var passLength = 9,
-      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*",
-      charVal = "";
-  for (var i = 0, n = charset.passLength; i < passLength; ++i) {
-      charVal += charset.charAt(Math.floor(Math.random() * n));
+  var length = answer,
+    charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*",
+    retVal = "";
+
+  for (i = 0, n = charset.length; i < length; ++i) {
+  retVal += charset.charAt(Math.floor(Math.random() * n));
   }
-  return charVal;
+  return retVal;
 }
