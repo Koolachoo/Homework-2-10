3ccf5f709ea752f4c69fe864aa2be1a5d35f3327
Justin Taylor <kiradez211@gmail.com>
2020-02-10 01:40:41 -0500

    Attempt at character type confirms

--- Develop/script.js
+++ Develop/script.js
@@ -7,15 +7,43 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   answer = prompt("What is the length of your password?");
+  if (answer < 8) {
+    alert("Password too short, enter a number greater than or equal to 128 characters.");
+    writePassword()
+  } else if (answer > 128) {
+    alert("Password is too long, enter a number less than or equal to 128 characters.");
+    writePassword()
+  } else {
    lcConfirm = confirm("Do you want lowercase characters");
+   if (lcConfirm){
+     charLow = "abcdefghijklmnopqrstuvwxyz"
+   }
+   else{
+    charLow = ""}
    upConfirm = confirm("Do you want uppercase characters");
+   if (upConfirm){
+    charUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  }
+  else{
+    charUp = ""}
    numConfirm = confirm("Do you want numeric characters");
+   if (numConfirm){
+    charNum = "0123456789"
+  }
+  else {
+    charNum = ""}
    spConfirm = confirm("Do you want special characters");
+   if (spConfirm){
+    charSpec = "!@#$%^&*"
+  }
+  else {
+    charSpec = ""}
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  }
 }
 
 // var lPrompt = var lcConfirm = var upConfirm = var numConfirm = var spConfirm = 
@@ -32,7 +60,7 @@ generateBtn.addEventListener("click", writePassword);
 // loop that contains the string for all of the characters
 function generatePassword() {
   var length = answer,
-    charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*",
+    charset = "charSpec" + "charNum" + "charUp" + "charLow",
     retVal = "";
 
   for (i = 0, n = charset.length; i < length; ++i) {
